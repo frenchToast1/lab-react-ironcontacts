8963f1f438957aae809310e48407ee998a692b0c
frenchToast <juliekogan1@gmail.com>
2022-01-15 11:55:39 +0100

    wip1&2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react/cjs/react.production.min";
+import contacts from "./contacts.json";
+
+/*
+class Contacts extends Component {
+
+   state = {
+    contacts: contacts.slice(0, 5),
+  };
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>picture</th>
+              <th>name</th>
+              <th>popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((el) => {
+              return (
+                <tr>
+                  <td>
+                    <img src={el.pictureUrl} alt="" />{" "}
+                  </td>
+                  <td>{el.name}</td>
+                  <td>{el.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contacts />
     </div>
-  );
+  )
+}
+*/
+
+class App extends Component {
+  state = {
+    contacts: contacts.slice(0, 5), // [ {}, {}, {}, {}, {} ]
+  };
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            console.log("clickk");
+
+            // 1. tirer au sort un contact de la liste -> Math.random()
+            const randomContact =
+              contacts[Math.floor(Math.random() * (contacts.length + 1))];
+
+            // 2. faire une copie de this.state.contacts
+            let newContacts = this.state.contacts.slice();
+
+            // 3. .push notre 1. dans notre copie
+            newContacts.push(randomContact);
+
+            // 4. .setState()
+            this.setState({
+              contacts: newContacts,
+            });
+          }}
+        >
+          {" "}
+          Add random contacts{" "}
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <th>picture</th>
+              <th>name</th>
+              <th>popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((el) => {
+              return (
+                <tr>
+                  <td>
+                    <img class="pic" src={el.pictureUrl} alt="" />{" "}
+                  </td>
+                  <td>{el.name}</td>
+                  <td>{el.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
